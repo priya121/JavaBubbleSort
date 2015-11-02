0ef376e2556d564f78a8be26c551a619e30e4406
priya121 <priya121@github.com>
2015-11-02 13:14:55 +0000

    Changed name of unsortedList to listOfNumbers.

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -2,16 +2,16 @@ import java.util.List;
 
 public class BubbleSort {
 
-    public List<Integer> sort(List<Integer> unsortedList) {
-        for (int i = 0; i < unsortedList.size(); i++) {
-            for (int j = 0; j < unsortedList.size(); j++) {
-                while (unsortedList.get(i) < (unsortedList.get(j))) {
-                    int smallerNumber = unsortedList.get(i);
-                    unsortedList.set(i, unsortedList.get(j));
-                    unsortedList.set(j, smallerNumber);
+    public List<Integer> sort(List<Integer> listOfNumbers) {
+        for (int i = 0; i < listOfNumbers.size(); i++) {
+            for (int j = 0; j < listOfNumbers.size(); j++) {
+                while (listOfNumbers.get(i) < (listOfNumbers.get(j))) {
+                    int smallerNumber = listOfNumbers.get(i);
+                    listOfNumbers.set(i, listOfNumbers.get(j));
+                    listOfNumbers.set(j, smallerNumber);
                 }
             }
         }
-        return unsortedList;
+        return listOfNumbers;
     }
 }
