a3e670db22723aa16cd9444c1fc5ee5639e9620e
priya121 <priya121@github.com>
2015-11-02 13:18:11 +0000

    Dried tests.

--- src/BubbleSortTest.java
+++ src/BubbleSortTest.java
@@ -7,22 +7,20 @@ import static junit.framework.TestCase.assertEquals;
 
 public class BubbleSortTest {
     BubbleSort bSort = new BubbleSort();
+    List<Integer> unsortedList = new ArrayList<>();
+    List<Integer> sortedList = new ArrayList<>();
 
     @Test
     public void sortsOne() {
-        List<Integer> unsortedList = new ArrayList<>();
         unsortedList.add(1);
-        List<Integer> sortedList = new ArrayList<>();
         sortedList.add(1);
         assertEquals(sortedList, bSort.sort(unsortedList));
     }
 
     @Test
     public void sortsTwo() {
-        List<Integer> unsortedList = new ArrayList<>();
         unsortedList.add(2);
         unsortedList.add(1);
-        List<Integer> sortedList = new ArrayList<>();
         sortedList.add(1);
         sortedList.add(2);
         assertEquals(sortedList, bSort.sort(unsortedList));
@@ -30,29 +28,25 @@ public class BubbleSortTest {
 
     @Test
     public void sortsThree() {
-        List<Integer> unsortedList = new ArrayList<>();
         unsortedList.add(3);
         unsortedList.add(2);
         unsortedList.add(1);
-        List<Integer> sortedList = new ArrayList<>();
         sortedList.add(1);
         sortedList.add(2);
         sortedList.add(3);
-        assertEquals(sortedList, new BubbleSort().sort(unsortedList));
+        assertEquals(sortedList, bSort.sort(unsortedList));
     }
 
     @Test
     public void sortsFour() {
-        List<Integer> unsortedList = new ArrayList<>();
         unsortedList.add(4);
         unsortedList.add(3);
         unsortedList.add(2);
         unsortedList.add(1);
-        List<Integer> sortedList = new ArrayList<>();
         sortedList.add(1);
         sortedList.add(2);
         sortedList.add(3);
         sortedList.add(4);
-        assertEquals(sortedList, new BubbleSort().sort(unsortedList));
+        assertEquals(sortedList, bSort.sort(unsortedList));
     }
 }
